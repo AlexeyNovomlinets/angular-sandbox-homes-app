7beb11f39fc252f87ecfe467fdd5a9df0fdb0369
Aliaksei Navamlinets <aliaksei_navamlinets@epam.com>
2023-08-01 15:58:49 +0300

    [feat]: step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<h1>Hello Universe!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  // TODO: Deploy it to gh-pages
+  title = signal('homes');
 }
