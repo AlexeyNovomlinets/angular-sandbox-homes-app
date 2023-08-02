cb05cf7b319cd8b168108b82fa38f659e84cfd57
Aliaksei Navamlinets <aliaksei_navamlinets@epam.com>
2023-08-02 16:42:23 +0300

    [feat]: add log effect

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/routes';
-import { counterReducer } from 'src/app/store/counter.reducers';
+import { CounterEffects } from './app/store/counter.effects';
+import { counterReducer } from './app/store/counter.reducers';
 
 const REDUCERS_MAP = {
   counter: counterReducer,
 };
 
+const EFFECTS = [CounterEffects];
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideStore(REDUCERS_MAP),
-]
-})
-  .catch(err => console.error(err));
+    provideEffects(EFFECTS),
+  ],
+}).catch(err => console.error(err));
